10f95d247890f9c4d27bbb15665093135b1ff657
Kevin Swallow <kevinswallow2@hotmail.com>
2011-10-29 18:34:48 -0700

    pract

--- pract.rb
+++ pract.rb
@@ -1,53 +1,7 @@
-# This program will
-#maybe we create the file if it doesn't already exist
-# 0. load the practice_sets file into an array of arrays
-# 1. display a list of practice sets to choose from
-# 2. ask the user which practice set he wants to do
-# 4. calculate a recommended practice sequence for this practice session
-#    (i.e. prioritize the routines list)
-# 5. display the recommended practice sequence to the user
-# 5a. each item in the sequence should have a link to a file describing what it is (this could be a text file or a video, or a set of videos in a folde:c)
-# 5b. each item in the sequence should have an opportunity for the user to input information (whether the user practiced it, whether the practice was successful, maybe a rating of how successful the practice was, maybe whether the practice was videorecorded)
-# 5c. it might be nice to tell the user statistical stuff about the practice history of that routine
-#6 enable the user to quit the program
-#7 save the user input for the next session
-#-----------------------------------------------------------------------------------------------
-#File Format
-#number of sets = sets.length => used by marshal
-#practicesets = array of practice set objects
-#  PracticeSet objects
-#    set name = set.name
-#    number of practice sessions = set.practice_count
-#    number of routines = set.routines.length => used by marshal
-#    [set of routines] = set.routines[0...routine_count]
-#    #Routine objects
-#	      name, = set.routines[i].name
-#	      link, = set.routines[i].link
-#	      priority  = set.routines[i].priority
-#	      practice count = set.routines[i].practice_count
-#	      success count = set.routines[i].success_count
-#	      set practice count at last practice = set.routines[i].last_routines_practice_count
-#	      last attempt successful? = set.routines[i].last_success_value
-#	      last time practiced  = set.routines[i].last_date_practiced
-#       score = set.routines[i].score
-#-------------------------------------------------------------------
-####Todo how do I handle stuff in series?
-####Todo track camera usage?
-
 ################TODO*****IN PROCESS*****TODO#################
-#TODO test addroutine
 #TODO make sure exits save properly
-#TODO can widget buttons change color or something when clicked?   Also, activate input boxes when button clicked (add routine, add set)
+#TODO can widget buttons change color or something when clicked?
 #TODO clean up variable scope in widgets
-#TODO reorder method definitions
-#Keith's suggestions
-# split trainer between object management and training
-#	consider getting rapid gui development with qt ruby by pragmatic press
-#	may be available fromdemonoid
-#	test driven development
-#
-
-#add note: git test
 
 require_relative 'marshaller'
 require_relative 'addset'
@@ -74,15 +28,14 @@ class Trainer
   include Marshaller
   include Os
   include ObjectManager
-####  include widgets
 
   def main #called at startup
     practice_sets, practice_set_names = get_practice_sets
     choose_set_to_practice(practice_sets, practice_set_names)
     MessageBoxWidget.run_qt("Returned to main at exit")
-#      write_practice_sets practice_sets     #TODO PM delete?
+#      write_practice_sets practice_sets
   end
-                                              #TODO adjust widget screen locations
+
   def choose_set_to_practice(practice_sets, practice_set_names) #called by main and recursed via new_set, add_a_set
     chosen_set_name, option = ChooseSetToPracticeWidget.run_qt(practice_set_names)
     if option == :add_set then get_new_set(practice_sets, practice_set_names) end
@@ -136,7 +89,6 @@ class Trainer
     sets_to_delete, status = SetsToDeleteWidget.run_qt(practice_set_names)
     if status == :quit then at_exit(practice_sets) end
     if (sets_to_delete == nil or status == :back) then choose_set_to_practice(practice_sets, practice_set_names) end
-#    confirmed = ConfirmDeleteWidget.run_qt(sets_to_delete) #TODO: use ButtonBox instead?
     if confirm_delete(sets_to_delete) == :no then choose_set_to_practice(practice_sets, practice_set_names) end
     delete_sets(sets_to_delete, practice_sets, practice_set_names)
       choose_set_to_practice(practice_sets, practice_set_names)
@@ -181,7 +133,6 @@ class Trainer
     if status == :quit then index_session_count(routines_in_process, initial_set_size, chosen_set, practice_sets) end
     if status == :back then practice_routines(chosen_set, practice_sets, practice_set_names, initial_set_size, routines_in_process) end
     if routines_to_delete == (nil or []) then practice_routines(chosen_set, practice_sets, practice_set_names, initial_set_size, routines_in_process) end
-#    confirmed = ConfirmDeleteWidget.run_qt(routines_to_delete)
     if confirm_delete(routines_to_delete) == :no then delete_routine(routines_in_process, initial_set_size, chosen_set, practice_sets, practice_set_names) end
     delete_routines(routines_in_process, chosen_set, routines_to_delete, initial_set_size)
   end
